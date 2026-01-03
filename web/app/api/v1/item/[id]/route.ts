89de5cf656b5aa8b93b28c5561124a065988709d
Zidan-09 <samuel.profissional18@gmail.com>
2026-01-03 12:54:39 -0300

    refactor: remove unused import for EditItem in route.ts

--- web/app/api/v1/item/[id]/route.ts
+++ web/app/api/v1/item/[id]/route.ts
@@ -1,6 +1,5 @@
 import { ItemController } from "@/app/lib/controllers/itemController";
 import { ItemMiddleware } from "@/app/lib/middlewares/itemMiddleware";
-import { EditItem } from "@/app/lib/utils/requests/itemRequest";
 import { withError } from "@/app/lib/withError";
 
 export const GET = withError(async (_: Request, ctx: any) => {
